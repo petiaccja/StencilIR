e0f0f2f86530e56b5acc3123f46f54f190bd4f77
Péter Kardos <kardospeter1994@hotmail.com>
2023-04-26 11:10:30 +0200

    remove old bufferization hack

--- src/Dialect/BufferizationExtensions/Transforms/OneShotBufferizeCombined.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <mlir/Dialect/Bufferization/Transforms/Passes.h>
-
-
-std::unique_ptr<mlir::Pass> createOneShotBufferizeCombinedPass(const mlir::bufferization::OneShotBufferizationOptions& options);
